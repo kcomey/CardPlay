2c3b06b3b5434708787ba68fca33f5b08970133a
Daniel Esqueda <dan.a.esqueda@gmail.com>
2015-05-18 11:22:56 -0700

    pulled

--- src/solitaire/actions.js
+++ src/solitaire/actions.js
@@ -3,3 +3,23 @@
 exports.draw = function(game) {
   return game;
 }
+
+exports.flip = function(game) {
+  return game;
+}
+
+exports.move = function(card, toStack, game) {
+  return game;
+}
+
+exports.promote = function(card, game) {
+  return game;
+}
+
+exports.unpromote = function(card, toStack, game) {
+  return game;
+}
+
+exports.reveal = function(stack, game) {
+  return game;
+}
--- src/solitaire/game.js
+++ src/solitaire/game.js
@@ -1,10 +1,13 @@
 'use strict';
 
-exports.create = function() {
+exports.create = function(options) {
+  options = options || {};
+  var drawSize = options.draw || 3;
+
   return {
     "id": "ac4f58e6c8e986b6d2f0a2f2041fc58a",
     "game": "solitaire",
-    "options": { "draw": 3 },
+    "options": { "draw": drawSize },
     "deck": [29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52],
     "draw": [],
     "stacks": [
