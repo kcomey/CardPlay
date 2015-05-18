e3b8653e7be3b2b22e290c70a1bfa1402123b911
Alec Hollingsworth <alec@mailc.net>
2015-05-18 13:46:41 -0700

    change the data structure representing a game

--- src/solitaire/game.js
+++ src/solitaire/game.js
@@ -5,29 +5,29 @@ exports.create = function(options) {
   var drawSize = options.draw || 3;
 
   return {
-    "id": "ac4f58e6c8e986b6d2f0a2f2041fc58a",
-    "game": "solitaire",
-    "options": { "draw": drawSize },
+    "options": {
+      "draw": drawSize,
+      "game": "solitaire",
+      "id": "ac4f58e6c8e986b6d2f0a2f2041fc58a",
+     },
     "deck": [29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52],
-    "draw": [],
-    "stacks": [
-      { "visible": [1], "hidden": [] },
-      { "visible": [8], "hidden": [2] },
-      { "visible": [14], "hidden": [3, 9] },
-      { "visible": [19], "hidden": [4, 10, 15] },
-      { "visible": [23], "hidden": [5, 11, 16, 20] },
-      { "visible": [26], "hidden": [6, 12, 17, 21, 24] },
-      { "visible": [28], "hidden": [7, 13, 18, 22, 25, 27] },
-    ],
-    "promoted": [
-      [],
-      [],
-      [],
-      [],
-    ],
+    "stacks": {
+      "draw": [],
+      "clubs": [],
+      "diamonds": [],
+      "spades": [],
+      "hearts": [],
+      0: { "visible": [1], "hidden": [] },
+      1: { "visible": [8], "hidden": [2] },
+      2: { "visible": [14], "hidden": [9, 3] },
+      3: { "visible": [19], "hidden": [15, 10, 4] },
+      4: { "visible": [23], "hidden": [20, 16, 11, 5] },
+      5: { "visible": [26], "hidden": [24, 21, 17, 12, 6] },
+      6: { "visible": [28], "hidden": [27, 25, 22, 18, 13, 7] },
+    },
   };
 }
 
-exports.validate = function() {
+exports.validate = function(game) {
   return true;
 }
