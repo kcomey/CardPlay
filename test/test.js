de43bde7f8e6fecdbbaf2532bc9ae04795fb188d
Alec Hollingsworth <alec@mailc.net>
2015-05-18 11:04:51 -0700

    fix game.create() to create deck with 24 cards, add 2 basic tests

--- test/test.js
+++ test/test.js
@@ -1,28 +1,16 @@
-//var myBitmap = require('../bitmap_header_module.js');
-//var expect = require('chai').expect;
+'use strict';
 
+var expect = require('chai').expect;
 
+describe('Solitaire', function() {
+  var game = require('../src/solitaire/game');
+  var actions = require('../src/solitaire/actions');
 
-describe('comparing bitmapObject.palette to bitmapObject.newPalette', function() {
-  //Returns bitmapObject.palette
-  //var original = myBitmap.readPalette();
-  //Returns bitmapObject.newPalette
-  //var transformed = myBitmap.transformPalette();
-
-  it('expect transformed to be different than original', function () {
-    //Should not be a copy of the data
-    //expect(transformed).to.not.equal(original);
+  it('should create a valid game', function () {
+    expect(game.validate(game.create())).to.be.true;
   });
 
-  it('expect length of both properties to be the same', function () {
-    //But should be same size
-    //expect(original.length).to.equal(transformed.length);
+  it('should create games with 24 cards in the deck', function () {
+    expect(game.create().deck.length).to.equal(24);
   });
 });
-
-
-
-
-
-
-
