5603e9864ce2596301f3bd97f6c70a9d6d44a12e
Kendall Comey <kendall.comey@gmail.com>
2015-05-19 11:20:17 -0700

    Have passport and keygen check working. Still need to send api-key in headers

--- src/router.js
+++ src/router.js
@@ -1,18 +1,41 @@
 var authenticateUser = require('./authenticate.js');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var authenticateUser = require('./authenticate.js');
+require('./config_passport')(passport);
 
 module.exports = function router(app) {
+
+app.use(passport.initialize());
+app.use(passport.session());
 // This is middleware
-//app.use(bodyParser.json());
+app.use(bodyParser.json());
 app.use(function (req, res, next) {
   authenticateUser.isAuthenticated(req, res, next);
 });
 
 app.get('/login', function(req, res) {
   // Authenticate the user here
-  res.send('This will be where the user can login');
+  console.log('This will be where the user can login');
 });
 
-app.route('/solitaire')
+app.post('/login',
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    // User has successfully logged in here, send wherever you want them to go
+    res.status(200).send('Logged in');
+    //res.redirect('/');
+  });
+
+  app.get('/solitaire', function(req, res) {
+  // Send to draw function
+    console.log('not getting here?');
+    res.status(200).send('Checking authentication, this works')
+  })
+
+
+// Needs to be fixed, changed, deleted, whatever
+/*app.route('/solitaire')
   app.post('/draw', function(req, res) {
   // Send to draw function
   })
@@ -30,5 +53,5 @@ app.route('/solitaire')
   })
   app.post('/reveal', function(req, res) {
   // Send to reveal function
-  });
+  });*/
 };
