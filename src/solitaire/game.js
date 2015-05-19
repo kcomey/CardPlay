3201452dab54cceb0c7b8aa603896db6efa7795d
Daniel Esqueda <dan.a.esqueda@gmail.com>
2015-05-19 09:06:18 -0700

    added shuffle function and test

--- src/solitaire/game.js
+++ src/solitaire/game.js
@@ -5,13 +5,15 @@ exports.shuffleDeck = function() {
   14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
   27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
   40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52];
+
   var m = 52, t, i;
 
   // While there remain elements to shuffle…
   while (m) {
 
     // Pick a remaining element…
-    i = Math.floor(Math.random() * m--);
+    i = Math.floor(Math.random() * m);
+    m -= 1;
 
     // And swap it with the current element.
     t = deck[m];
