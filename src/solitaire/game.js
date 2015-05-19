3d33824affccd9cc8cdf3632032defe2321e4d44
Alec Hollingsworth <alec@mailc.net>
2015-05-19 09:26:02 -0700

    add game.createDeck(kind) function for generating decks

--- src/solitaire/game.js
+++ src/solitaire/game.js
@@ -23,9 +23,23 @@ exports.shuffleDeck = function() {
   return deck;
 }
 
+exports.createDeck = function(kind) {
+  if (kind === 'shuffled') {
+    return this.shuffleDeck();
+  } else {
+    return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+  14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+  27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+  40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52];
+  }
+}
+
 exports.create = function(options) {
   options = options || {};
   var drawSize = options.draw || 3;
+
+  options.deck = options.deck || 'shuffled';
+  var deck = this.createDeck(options.deck);
 
   return {
     "options": {
