3b4a641b22f18dd4510314163608c609c9b1d308
Kendall Comey <kendall.comey@gmail.com>
2015-05-19 09:21:37 -0700

    Working but not finished

--- src/authenticate.js
+++ src/authenticate.js
@@ -1,14 +1,33 @@
 var mongo = require('./mongoose.js');
+var keygen = require('keygen');
+var LocalStrategy = require('passport-local').Strategy;
 
-module.exports =
-  function(username, password) {
-    mongo.User.findOne({ username: username }, function (err, user) {
-      if (err) {
-      console.log('Error!');
+module.exports.authenticate = function(username, password) {
+  mongo.User.findOne({ username: username }, function (err, user) {
+    if (err) {
+    console.log('Error!');
+    }
+    if (password != user.password) {
+      console.log('Password is not correct');
+    }
+    var userKey = keygen.url(keygen.medium);
+    mongo.User.findOneAndUpdate({_id: user.id}, {keygen: userKey}, {new: true},
+      function(err, result) {
+        if (err) {
+          return done(null, false);
+        };
       }
-      if (password != user.password) {
-        console.log('Password is not correct');
-      }
-      console.log(user.username + ' is logged in');
-    });
-  };
+    );
+    console.log(user.username + ' is logged in');
+  });
+};
+
+module.exports.isAuthenticated = function(req, res, next) {
+  console.log('checking auth');
+  console.log(req.header);
+  next();
+};
+
+
+
+
--- src/server.js
+++ src/server.js
@@ -1,10 +1,30 @@
 var express = require('express');
-var expressStatic  = require('express-static');
-var session = require('express-session')
+var passport = require('passport');
 var app = express();
-var authenticate = require('./authenticate.js');
+var router = require('./router.js');
+var morgan = require('morgan');
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config_passport')(passport);
+var authenticateUser = require('./authenticate.js');
 
-authenticate('Kendall', 'Lucky7');
+router(app);
+
+// Setting up middleware
+//app.use(bodyParser.json());
+app.use(morgan('dev'));
+/*app.use(function (req, res, next) {
+  authenticateUser.isAuthenicated(req, res, next);
+});*/
+
+/*app.post('/login',
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    console.log('works to authenticate');
+    console.log(req.user);
+    res.status(200).send('Logged in');
+    //res.redirect('/');
+  });*/
 
 app.listen(process.env.PORT || 3000, function() {
   console.log('server started');
