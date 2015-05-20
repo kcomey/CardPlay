7ea6ca8c2958d7df49fff16104aa5fbb87a1fac4
Kendall Comey <kendall.comey@gmail.com>
2015-05-20 15:43:17 -0700

    Added switch statement to test /draw

--- src/authenticate.js
+++ src/authenticate.js
@@ -1,39 +1,15 @@
 var mongo = require('./mongoose.js');
 var keygen = require('keygen');
 
-exports.saveState = function(gameID, game) {
-  var key = game.options.id;
-  console.log(key);
-  console.log(gameID);
-  console.log(game);
+exports.saveState = function(game, callback) {
+  var gameID = game.options.id;
 
-  // mongo.Solitaire.findOneAndUpdate({"options.id": gameID}, game, {upsert: true, new: true},
-  mongo.Solitaire.findOneAndUpdate({"options.id": "100"}, game, {upsert: true, new: true},
-    function(err, result) {
-      console.dir(err);
-      console.log('thats the err');
-      console.dir(result);
-      console.log('thats the result');
-      if (err) {
-        res.status(404).send('Cannot save game');
-      }
-      result.save();
-    });
+  mongo.Solitaire.findOneAndUpdate({"options.id": gameID}, game,
+    {upsert: true, new: true}, callback);
 };
 
-exports.getState = function(req, res, gameID) {
-  console.log('getState: ', gameID);
-  mongo.Solitaire.findOne({ "options.id": gameID }, function (err, game) {
-    if (err) {
-      res.status(404).send('Game not found');
-    }
-    var form = '<form action="/solitaire/game/' + gameID + '" method="post">';
-    form += '<br>Username: <input type="text" name="user" value="Username">';
-    form += '<br>Password: <input type="password" name="pass">';
-    form += '<br><input type="submit" name="login" value="Log In">';
-    form += '</form>\n';
-    res.status(200).send(form + game);
-  });
+exports.getState = function(gameID, callback) {
+  mongo.Solitaire.findOne({ "options.id": gameID }, callback);
 };
 
 exports.setSession = function(req, res, callback) {
