2bd0fddadd14c73c17009112765e92dee6eda058
Kendall Comey <kendall.comey@gmail.com>
2015-05-21 15:28:58 -0700

    Style errors corrected, passed jshint

--- src/authenticate.js
+++ src/authenticate.js
@@ -4,12 +4,12 @@ var keygen = require('keygen');
 exports.saveState = function(game, callback) {
   var gameID = game.options.id;
 
-  mongo.Solitaire.findOneAndUpdate({"options.id": gameID}, game,
+  mongo.Solitaire.findOneAndUpdate({'options.id': gameID}, game,
     {upsert: true, new: true}, callback);
 };
 
 exports.getState = function(gameID, callback) {
-  mongo.Solitaire.findOne({ "options.id": gameID }).lean().exec(callback);
+  mongo.Solitaire.findOne({ 'options.id': gameID }).lean().exec(callback);
 };
 
 exports.setSession = function(req, res, callback) {
@@ -20,28 +20,31 @@ exports.setSession = function(req, res, callback) {
     function(err, result) {
       callback(err, userKey);
     }
-  )
+  );
 };
 
 exports.isAuthenticated = function(req, res, next) {
   // If they are just logging in they don't need to be authenticated yet
-  if (req.path === '/login') return next();
-   // var token = req.session.token;
-   var token = req.cookies.session;
-   mongo.User.findOne({ session: token },function(err, user) {
-      console.log('user is ' + user);
-      if (err) {
-        return err;
-      }
-      // If key is found, send them on their way
-      if (user) {
-        next();
-      }
-      else {
-        // Are not authorized
-        res.redirect('/login');
-      }
-    });
+  if (req.path === '/login') {
+    return next();
+  }
+  var token = req.cookies.session;
+  if (token === undefined) {
+    res.redirect('/login');
+  }
+  mongo.User.findOne({ session: token }, function(err, user) {
+    console.log('user is ' + user);
+    if (err) {
+      return err;
+    }
+    // If key is found, send them on their way
+    if (user) {
+      next();
+    } else {
+      // Are not authorized
+      res.redirect('/login');
+    }
+  });
 };
 
 
--- src/solitaire/game.js
+++ src/solitaire/game.js
@@ -4,11 +4,13 @@ var keygen = require('keygen');
 
 exports.shuffleDeck = function() {
   var deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-  14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
-  27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-  40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52];
+    14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+    27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+    40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52,
+  ];
 
-  var m = 52, t, i;
+  var m = 52;
+  var t, i;
 
   // While there remain elements to shuffle…
   while (m) {
@@ -30,19 +32,22 @@ exports.createDeck = function(kind) {
     return this.shuffleDeck();
   } else if (kind === 'special') {
     return [8, 15, 3, 4, 5, 6, 7, 1, 9, 10, 11, 12, 13,
-  14, 2, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
-  27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-  40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52];
+      14, 2, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+      27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+      40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52,
+    ];
   } else if (kind === 'chain') {
     return [44, 26, 3, 19, 5, 8, 7, 6, 9, 10, 11, 12, 13,
-  18, 15, 23, 17, 28, 4, 30, 21, 22, 16, 24, 25, 2,
-  27, 14, 29, 20, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-  40, 41, 42, 43, 1, 45, 46, 47, 48, 49, 50, 51, 52];
+      18, 15, 23, 17, 28, 4, 30, 21, 22, 16, 24, 25, 2,
+      27, 14, 29, 20, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+      40, 41, 42, 43, 1, 45, 46, 47, 48, 49, 50, 51, 52,
+    ];
   } else {
     return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-  14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
-  27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-  40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52];
+      14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+      27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+      40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52,
+    ];
   }
 };
 
@@ -54,43 +59,67 @@ exports.create = function(options) {
   var deck = this.createDeck(options.deck);
 
   var game = {
-    "options": {
-      "draw": drawSize,
-      "game": "solitaire",
-      "id": keygen.hex(keygen.small),
-     },
-    "deck": deck,
-    "stacks": {
-      "draw": [],
-      "clubs": [],
-      "diamonds": [],
-      "spades": [],
-      "hearts": [],
-      0: { "visible": [], "hidden": [] },
-      1: { "visible": [], "hidden": [] },
-      2: { "visible": [], "hidden": [] },
-      3: { "visible": [], "hidden": [] },
-      4: { "visible": [], "hidden": [] },
-      5: { "visible": [], "hidden": [] },
-      6: { "visible": [], "hidden": [] },
+    options: {
+      draw: drawSize,
+      game: 'solitaire',
+      id: keygen.hex(keygen.small),
+    },
+    deck: deck,
+    stacks: {
+      draw: [],
+      clubs: [],
+      diamonds: [],
+      spades: [],
+      hearts: [],
+      0: {
+        visible: [],
+        hidden: [],
+      },
+      1: {
+        visible: [],
+        hidden: [],
+      },
+      2: {
+        visible: [],
+        hidden: [],
+      },
+      3: {
+        visible: [],
+        hidden: [],
+      },
+      4: {
+        visible: [],
+        hidden: [],
+      },
+      5: {
+        visible: [],
+        hidden: [],
+      },
+      6: {
+        visible: [],
+        hidden: [],
+      },
     },
   };
 
   if (options.deck === 'easy') {
     game.deck = [];
     game.stacks.clubs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].reverse();
-    game.stacks.diamonds = [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26].reverse();
-    game.stacks.spades = [27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39].reverse();
-    game.stacks.hearts = [40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51].reverse();
+    game.stacks.diamonds = [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+    .reverse();
+    game.stacks.spades = [27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39]
+    .reverse();
+    game.stacks.hearts = [40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51]
+    .reverse();
     game.stacks[0].visible = [52];
     return game;
   }
 
   for (var i = 0; i <= 6; i++) {
     for (var j = i; j <= 6; j++) {
-      if (j === i) { // deal a visible card to stack i
+      if (j === i) { // Deal a visible card to stack i
         game.stacks[j].visible.unshift(deck.shift());
-      } else { // deal a hidden card to stack i
+      } else { // Deal a hidden card to stack i
         game.stacks[j].hidden.unshift(deck.shift());
       }
     }
@@ -110,18 +139,28 @@ exports.getRank = function(card) {
 };
 
 exports.getSuit = function(card) {
-  if ( 1 <= card && card <= 13) { return "clubs"; }
-  else if (14 <= card && card <= 26) { return "diamonds"; }
-  else if (27 <= card && card <= 39) { return "spades"; }
-  else if (40 <= card && card <= 52) { return "hearts"; }
+  if (1 <= card && card <= 13) {
+    return 'clubs';
+  } else if (14 <= card && card <= 26) {
+    return 'diamonds';
+  } else if (27 <= card && card <= 39) {
+    return 'spades';
+  } else if (40 <= card && card <= 52) {
+    return 'hearts';
+  }
   return null;
 };
 
 exports.getColor = function(card) {
-  if ( 1 <= card && card <= 13) { return "black"; }
-  else if (14 <= card && card <= 26) { return "red"; }
-  else if (27 <= card && card <= 39) { return "black"; }
-  else if (40 <= card && card <= 52) { return "red"; }
+  if (1 <= card && card <= 13) {
+    return 'black';
+  } else if (14 <= card && card <= 26) {
+    return 'red';
+  } else if (27 <= card && card <= 39) {
+    return 'black';
+  } else if (40 <= card && card <= 52) {
+    return 'red';
+  }
   return null;
 };
 
@@ -148,23 +187,25 @@ exports.validMove = function(game, card, from, to) {
     return true;
 
 
-  } else { return false; }
+  } else {
+    return false;
+  }
 };
 
 exports.cloneStacks = function(stacks) {
   return {
-    "draw": stacks.draw,
-    "clubs": stacks.clubs,
-    "diamonds": stacks.diamonds,
-    "spades": stacks.spades,
-    "hearts": stacks.hearts,
+    draw: stacks.draw,
+    clubs: stacks.clubs,
+    diamonds: stacks.diamonds,
+    spades: stacks.spades,
+    hearts: stacks.hearts,
     0: stacks[0],
     1: stacks[1],
     2: stacks[2],
     3: stacks[3],
     4: stacks[4],
     5: stacks[5],
-    6: stacks[6]
+    6: stacks[6],
   };
 };
 
@@ -172,7 +213,7 @@ exports.cloneGame = function(game) {
   return {
     options: game.options,
     deck: game.deck,
-    stacks: this.cloneStacks(game.stacks)
+    stacks: this.cloneStacks(game.stacks),
   };
 };
 
