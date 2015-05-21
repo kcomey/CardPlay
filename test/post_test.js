ea39511e4387b2e2fc578ffda1a6e43a5622ee8a
Alec Hollingsworth <alec@mailc.net>
2015-05-21 15:14:13 -0700

    add some agent (w/ cookies) POST tests

--- test/post_test.js
+++ test/post_test.js
@@ -18,7 +18,6 @@ describe('The API responds to GETs & POSTs', function() {
         if (err) {
           return err;
         }
-        // console.log('user daniel created');
         done();
       });
   });
@@ -30,6 +29,24 @@ describe('The API responds to GETs & POSTs', function() {
       }
       done();
     });
+  });
+
+  it('should not be able to create new games if not logged in', function(done) {
+    agent.get('/solitaire/newgame').redirects(0).end(function(err, res) {
+      expect(res).to.redirectTo('/login');
+      done();
+    });
+  });
+
+  it('should not log in with bad password', function(done) {
+    agent
+      .post('/login')
+      .redirects(0)
+      .send({username: 'daniel', password: 'hunter2'})
+      .end(function(err, res) {
+        expect(res).to.redirectTo('/login');
+        done();
+      });
   });
 
   it('should log in testUser and return User is logged in', function(done) {
@@ -51,18 +68,34 @@ describe('The API responds to GETs & POSTs', function() {
     });
   });
 
-  it('should be able to create new games', function(done) {
-    agent.get('/solitaire/newgame').redirects(0).end(function(err, res) {
-      expect(res).to.have.status(302);
-      expect(res.headers.location.match(/^\/solitaire\/game\/.*/)).to.not.be.null;
-      agent
-        .post(res.headers.location)
-        .send({ action: "draw" })
-        .end(function(err, res) {
-          if (err) { return err; }
-          expect(res).to.have.status(200);
-          done();
-        });
-    });
-  });
+  // it('should be able to create new games', function(done) {
+  //   var gameURL;
+  //   agent
+  //     .get('/solitaire/newgame')
+  //     .then(function(res) {
+  //       gameURL = res.location;
+  //     })
+  //     .post(gameURL)
+  //     .send({ action: "draw" })
+  //     .end(function(err, res) {
+  //       if (err) { return err; }
+  //       expect(res).to.have.status(200);
+  //       done();
+  //     });
+  // });
+
+  // it('', function(done) {
+  //   agent.get('/solitaire/newgame').redirects(0).end(function(err, res) {
+  //     expect(res).to.have.status(302);
+  //     expect(res.headers.location.match(/^\/solitaire\/game\/.*/)).to.not.be.null;
+  //     agent
+  //       .post(res.headers.location)
+  //       .send({ action: "draw" })
+  //       .end(function(err, res) {
+  //         if (err) { return err; }
+  //         expect(res).to.have.status(200);
+  //         done();
+  //       });
+  //   });
+  // });
 });
