e6ca2722533c0cc2b1128bc75d3f62a2685919a7
Alec Hollingsworth <alec@mailc.net>
2015-05-21 09:52:44 -0700

    add the fixed router.js file omitted in previous commit

--- src/router.js
+++ src/router.js
@@ -98,6 +98,8 @@ app.post('/solitaire/game/:gameID', function(req, res) {
                 res.redirect('/solitaire/game/' + req.params.gameID);
               }
             })
+          } else {
+            // send 400
           }
           break;
 
