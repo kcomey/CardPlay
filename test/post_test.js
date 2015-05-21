9902997636a5ba88ba33675a6bf63aabaef44707
Alec Hollingsworth <alec@mailc.net>
2015-05-21 14:35:15 -0700

    add a test file for POSTing game actions

---
+++ test/post_test.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var app = require('../src/server');
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+var expect = chai.expect;
+var mongo = require('../src/mongoose.js');
+
+var agent = chai.request.agent(app);
+
+describe('The API responds to GETs & POSTs', function() {
+  before(function(done) {
+    mongo.User.findOneAndUpdate({username: 'daniel'},
+      {username: 'daniel', password: 'test'},
+      {upsert: true},
+      function(err, doc) {
+        if (err) {
+          return err;
+        }
+        // console.log('user daniel created');
+        done();
+      });
+  });
+
+  after(function(done) {
+    mongo.User.findOneAndRemove({username: 'daniel'}, function(err, doc) {
+      if (err) {
+        return err;
+      }
+      done();
+    });
+  });
+
+  it('should log in testUser and return User is logged in', function(done) {
+    agent
+      .post('/login')
+      .redirects(0)
+      .send({username: 'daniel', password: 'test'})
+      .end(function(err, res) {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+
+  it('should be able to create new games', function(done) {
+    agent.get('/solitaire/newgame').redirects(0).end(function(err, res) {
+      expect(res).to.have.status(302);
+      expect(res.headers.location.match(/^\/solitaire\/game\//)).to.not.be.null;
+      done();
+    });
+  });
+
+  it('should be able to create new games', function(done) {
+    agent.get('/solitaire/newgame').redirects(0).end(function(err, res) {
+      expect(res).to.have.status(302);
+      expect(res.headers.location.match(/^\/solitaire\/game\/.*/)).to.not.be.null;
+      agent
+        .post(res.headers.location)
+        .send({ action: "draw" })
+        .end(function(err, res) {
+          if (err) { return err; }
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+  });
+});
